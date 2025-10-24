59bc949d98bffc7321d8e7577eb74e1afae8fbe4
myaseen16 <your.email@example.com>
2025-10-25 00:47:12 +0500

    Stop-Watch-Application

---
+++ app.js
@@ -0,0 +1,68 @@
+console.log("JavaScript Connected");
+var display = document.getElementById("display");
+var startTimer = document.getElementById("start");
+var stopTimer = document.getElementById("stop");
+var resetTimer = document.getElementById("reset");
+
+var hours = 0;
+var minutes = 0;
+var seconds = 0;
+var timer = null;
+
+function showTime(){
+
+var h = hours;
+var m = minutes;
+var s = seconds;
+
+
+if(h < 10){
+    h = "0" + h
+}
+if(m < 10){
+    m = "0" + m
+}
+if(s < 10){
+    s = "0" + s
+}
+
+display.textContent = h + ":" + m + ":" + s;
+}
+
+function startTime(){
+
+if(timer !== null){
+    return;
+}
+
+timer = setInterval(function(){
+    seconds++;
+
+
+if(seconds === 60){
+    seconds = 0;
+    minutes++
+}
+if(minutes === 60){
+minutes = 0;
+hours++
+}
+
+showTime()
+
+}, 1000);
+}
+
+function stopTime(){
+    clearInterval(timer)
+    timer = null
+}
+function resetTime(){
+    clearInterval(timer);
+    timer = null;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+
+showTime()
+}
